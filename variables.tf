67d14e279b560c755595410a0c5dd4793ca8ed25
anson inomwan <inomwanosaruese@gmail.com>
2026-07-29 20:38:06 -0400

    Terraform GitHub Actions Lab

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "rg-github-actions-lab"
+}
+
+variable "location" {
+  default = "Canada Central"
+}
+
+variable "storage_account_name" {
+  default = "ghactionstorage3511"
+}
